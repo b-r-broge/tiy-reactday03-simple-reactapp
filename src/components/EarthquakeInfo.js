480a1f23ea335aba376b729e4cae1469fdb459d8
Brad Broge <brbroge@gmail.com>
2017-07-19 20:38:49 -0400

    completed hardmode calling remote json file

--- src/components/EarthquakeInfo.js
+++ src/components/EarthquakeInfo.js
@@ -4,7 +4,7 @@ class EarthquakeInfo extends Component {
   render() {
     return (
       <div className="earthquake-title">
-        <p> This is a list of 8 Earthquakes occurring on the morning of July 14th across the United States </p>
+        <p> This is a list of up to 8 Earthquakes occurring across the world </p>
       </div>
     )
   }
--- src/components/EarthquakeList.js
+++ src/components/EarthquakeList.js
@@ -1,29 +1,48 @@
 import React, { Component } from 'react';
 import moment from 'moment';
 
-import earthquakes from '../data/earthquakes'
 
 class EarthquakeList extends Component {
-  render() {
-    let earthList = earthquakes.features.map((quake) => {
-      return (
-        <div className="col-sm-6" key={quake.id}>
-          <div className="card" >
-            <div className="card-block">
-              <h4 className="card-title">{quake.properties.place}</h4>
-              <h6 className="card-subtitle mb-2 text-muted">Magnitude: {quake.properties.mag}</h6>
-              <h6 className="card-subtitle mb-2 text-muted">Time: {moment(quake.properties.time).format('llll')}</h6>
-              <p className="card-text">Coordinates: {quake.geometry.coordinates}</p>
-              <a href={quake.properties.url} className="card-link">USGS Event Link</a>
+  constructor () {
+    super()
+    this.state = {
+      earthList: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson')
+    .then(resp => resp.json())
+    .then((earthquakes) => {
+      // console.log('found earthquakes:', earthquakes);
+      let earthOut = earthquakes.features.map((quake) => {
+        return (
+          <div className="col-sm-6" key={quake.id}>
+            <div className="card" >
+              <div className="card-block">
+                <h4 className="card-title">{quake.properties.place}</h4>
+                <h6 className="card-subtitle mb-2 text-muted">Magnitude: {quake.properties.mag}</h6>
+                <h6 className="card-subtitle mb-2 text-muted">Time: {moment(quake.properties.time).format('llll')}</h6>
+                <p className="card-text">Coordinates: {quake.geometry.coordinates}</p>
+                <a href={quake.properties.url} className="card-link">USGS Event Link</a>
+              </div>
             </div>
           </div>
-        </div>
-      )
+        )
+      })
+      this.setState({earthList: earthOut})
     })
+    .catch((err) => {
+      console.log('There was an error importing usgs earthquake data:', err);
+    })
+  }
+
+  render() {
+    console.log('state of earthList:', this.state.earthList);
     return (
       <div className="quake-list">
         <div className="row">
-          {earthList}
+          {this.state.earthList}
         </div>
       </div>
     )
